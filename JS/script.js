26f7e6afaa4af96711d66aeaa76511dba6e6916c
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-06-10 18:04:21 +0200

    functions added

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,21 @@
-console.log("Hello World!😎")
+{
+    const welcome = () => {
+        console.log("Hello World!😎")
+    };
 
-let changeColorButton = document.querySelector(".js-changeColorButton");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".js-themeName");
+    const toggleBackground = () => {
+        const body = document.documentElement;
+        const themeName = document.querySelector(".js-themeName");
+        body.classList.toggle("body--dark");
+        themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+    };
 
-changeColorButton.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
+    const init = () => {
+        const changeColorButton = document.querySelector(".js-changeColorButton");
+        changeColorButton.addEventListener("click", toggleBackground);
+       
+        welcome();
+    };
 
-    themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
-});
+    init();
+}
